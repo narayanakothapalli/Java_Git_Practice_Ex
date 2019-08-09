b9097f895c293de658d5860b745cae1a2a13f4bf
narayanakothapalli <narayana.kothapalli@gmail.com>
2019-08-10 00:09:02 +0530

    intial commit

---
+++ src/StreamOne.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamOne {
+
+    public static void main(String args[]){
+
+
+        List<Integer> l1=new ArrayList<>();
+        l1.add(0);
+        l1.add(5);
+        l1.add(10);
+        l1.add(15);
+        l1.add(20);
+        l1.add(25);
+        l1.add(30);
+
+
+        List<Integer> finList=l1.stream().filter(i->i%2==0).collect(Collectors.toList());
+
+        System.out.println(finList);
+        System.out.println("Sample git command example");
+
+
+    }
+}
